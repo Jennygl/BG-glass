f2a47824f48e5b2dee39a612b0e03c2dc535f3c4
Tim Viklund Fjesme <tim.viklund.fjesme@iths.se>
2023-06-05 13:49:36 +0200

    Fixat en ny navbar, stylat POST component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,11 @@
 import styled from 'styled-components'
+import NavCream from '../assets/NavCream.png'
 
 function Navbar() {
   return (
     <>
     <Nav>
-        <Ul>
-
-
-      <Icon> <a className='cream' href='http://localhost:5173/'><ion-icon  name="ice-cream-outline"></ion-icon> </a></Icon>
-
-    <Text>BG-Glace</Text>
-    </Ul>
+      <Icon> <a className='cream' href='http://localhost:5173/'><img src={NavCream} alt="" /></a></Icon>
     </Nav>
 
     </>
@@ -19,46 +14,33 @@ function Navbar() {
 
 export default Navbar
 
-// NAV
 const Nav = styled.nav`
-position: absolute;
-z-index: 3;
-top:0px;
-height: 5.5rem;
-width: 100vw;
-background: linear-gradient(90deg, rgba(0, 0, 0, 0.6) 8.58%, rgba(0, 0, 0, 0) 59.62%);
-display: flex;
-@media screen and (max-width: 600px) {
-    background: linear-gradient(90deg, rgba(0, 0, 0, 0.45) 3.58%, rgba(0, 0, 0, 0) 120.62%);
-}
-
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
 `
 
-const Ul = styled.ul`
-display: flex;
-justify-content: start;
-align-items: center;
-padding-left: .4rem;
-`
-
-// NAV ICON
 const Icon = styled.li`
 font-size: 3.5rem;
 list-style: none;
 margin: .4rem .3rem 0 0;
-@media screen and (max-width: 600px) {
- font-size: 3rem;
-}
-`
 
-//NAV TEXT
-const Text = styled.li`
-font-family: 'Oregano';
-list-style: none;
-font-weight: 400;
-font-size: 3rem;
-color: #E690A3;
+img {
+    height: auto;
+    width: 12vw;
+}
+
+&:hover {
+    img {
+      filter: brightness(0.9);
+    }
+}
+
 @media screen and (max-width: 600px) {
-   font-size: 3rem;
+    img {
+        height: auto;
+        width: 20vw;
+    }
 }
 `
